d01a6e63a9c22c592dd755df0afafbfe1fbbab43
young <kug2001@gmail.com>
2021-04-30 22:00:14 +0900

    game codding finish

--- carrot/main.js
+++ carrot/main.js
@@ -3,7 +3,11 @@ const stopBtn = document.querySelector('.stop__btn');
 const timerBox = document.querySelector('.timer__box');
 const stage = document.querySelector('.stage__container');
 const countBox = document.querySelector('.count__box');
-const screenyMax = 500; 
+const restartBox = document.querySelector('.restart__box');
+const restartBtn = document.querySelector('.restart__btn');
+const resultBox = document.querySelector('.result__box');
+
+const screenyMax = 470; 
 const screenyMin = 0; 
 const screenxMax = window.screen.width-100;
 const screenxMin = 0; 
@@ -11,37 +15,40 @@ let mainCount = 10;
 
 function stopBtnEvent (timeId){
     stopBtn.addEventListener('click', ()=>{
-        clearInterval(timeId);
-        bugs.forEach(entry =>{
-            entry.remove();
-        });
-        carrots.forEach(entry =>{
-            entry.remove();
-        });
+        removeimg();
         stopBtn.classList.add('active');
         startBtn.classList.remove('active');
         timerBox.textContent = `00:10`;
+        return clearInterval(timeId);
     });
 };
 function carrotBtnEvent(timeId){
     stage.addEventListener('click', (event) => {
         const name = event.target.className;
-        const id = event.target.dataset.id
-        
-        console.log(event.target);
+        const id = event.target.dataset.id;
+        const toBeDeleted = document.querySelector(`.carrot[data-id="${id}"]`);
         if(name === 'bug'){
-            console.log("bug");
+            resultBox.textContent = "You Lost.";
+            restartBox.classList.remove('active');
+            clearInterval(timeId);
         }
-        else if(name === 'carrot'){
-            console.log("carrot");
-            const toBeDeleted = document.querySelector(`.carrot[data-id="${id}"]`);
-            toBeDeleted.remove();
-            mainCount--;
-            countBox.textContent = mainCount
-            console.log(mainCount);
-            if(mainCount === 0){
-                clearInterval(timeId);
+        else if(name === 'carrot' && id){
+            console.log(id); 
+            console.log(toBeDeleted); 
+            if(toBeDeleted){
+                toBeDeleted.remove();
+                mainCount--;
+                countBox.textContent = mainCount;
+                
             }
+        }
+        else if(mainCount === 0){
+            countBox.textContent = 0;
+            resultBox.textContent = "You Win";
+            restartBox.classList.remove('active');
+            stopBtn.classList.add('active');
+            startBtn.classList.remove('active');
+            return clearInterval(timeId);
         }
         else{
             return;
@@ -56,7 +63,9 @@ function countDownTimer(){
     timerBox.textContent = `00:10`;
     const timeId = setInterval(() => {
         timerBox.textContent = `00:0${time}`;
-        if(time <= 0){
+        if(time === 0){
+            resultBox.textContent = "시간이 초과되었습니다.";
+            restartBox.classList.remove('active');
             clearInterval(timeId);
         }
         else{
@@ -65,15 +74,15 @@ function countDownTimer(){
         stopBtnEvent(timeId);
         carrotBtnEvent(timeId);
     }, 1000);
-
 };
+
 function getRandomInt(max, min) {
     const minpx = Math.ceil(min);
     const maxpx = Math.floor(max);
     const randomNumber = Math.floor(Math.random() * (maxpx - minpx) + minpx);
-    // console.log(randomNumber);
     return randomNumber;
 };
+
 function creatImg (className, scrName){
     for(i=0 ; i<10 ; i++){
         let x, y;
@@ -89,6 +98,16 @@ function creatImg (className, scrName){
     }
 };
 
+function removeimg(){
+    const bug = document.querySelectorAll('.bug');
+    const carrot = document.querySelectorAll('.carrot');
+    bug.forEach(entry =>{
+        entry.remove();
+    });
+    carrot.forEach(entry =>{
+        entry.remove();
+    });
+};
 
 startBtn.addEventListener('click', (event) =>{
     startBtn.classList.add('active');
@@ -96,5 +115,15 @@ startBtn.addEventListener('click', (event) =>{
     creatImg("bug", "/img/bug.png");
     creatImg("carrot", "/img/carrot.png");
     countDownTimer();
+});
+
+restartBtn.addEventListener('click', (event) => {   
+    stopBtn.classList.add('active');
+    startBtn.classList.remove('active');
+    restartBox.classList.add('active');
+    timerBox.textContent = `00:00`;
+    countBox.textContent = 10;
+    mainCount = 10;
+    removeimg();
 });
 
